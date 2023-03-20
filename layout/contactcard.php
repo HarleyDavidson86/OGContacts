f9b86bc38ab3246aaea723a824d8821eda845d50
Dominik Sust <dominik.sust@googlemail.com>
2023-03-20 15:06:46 +0100

    Default contact card

---
+++ layout/contactcard.php
@@ -0,0 +1,10 @@
+<!-- Contact Card -->
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">{{name}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{position}}</h6>
+        <p class="card-text">E-Mail: <a href="mailto:{{email}}">{{email}}</a><br>
+            Phone: {{phone}}</p>
+    </div>
+</div>
+<!-- End -->
